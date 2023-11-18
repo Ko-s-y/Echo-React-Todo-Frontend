bb484c00f18f14bafd9626805e9577d0f8b97691
s-ko <song.1209@icloud.com>
2023-11-18 16:24:28 +0900

    chore: setup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Auth } from './components/Auth'
+import { Todo } from './components/Todo'
 
 function App() {
   return (
